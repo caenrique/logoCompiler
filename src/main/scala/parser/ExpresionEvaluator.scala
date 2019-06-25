b5c0d098af3abab43b2c38d76c2c61b38fc7deba
César Enrique Ramírez <caenrique93@gmail.com>
2019-06-25 13:00:57 +0200

    Implementados los comandos de mover
    + Añadidos dos metodos de utilidad a ExpresionEvaluator para evaluar
    numeros
    + añadido campo orientation a SymbolTable para tener registrada la
    orientacion en todo momento

--- src/main/scala/parser/ExpresionEvaluator.scala
+++ src/main/scala/parser/ExpresionEvaluator.scala
@@ -9,6 +9,85 @@ object ExpresionEvaluator {
 
   import Value._
 
+  def evalNumber(expr: Expresion, s: SymbolTable, errMsg: String = "se esperaba un numero")
+                (f: Int => EvalData)
+  : EvalData = {
+    ExpresionEvaluator(expr, s) match {
+      case Left(err) => EvalData(Left(err), s)
+      case Right(Left(number)) => f(number)
+      case _ => EvalData(Left(LogoEvaluationError(errMsg)), s)
+    }
+  }
+
+  def eval2Numbers(expr1: Expresion, expr2: Expresion, s: SymbolTable, errMsg: String = "se esperaban 2 numeros")
+                  (f: (Int, Int) => EvalData)
+  : EvalData = {
+    val ab = for {
+      aValue <- ExpresionEvaluator(expr1, s)
+      bValue <- ExpresionEvaluator(expr2, s)
+    } yield (aValue, bValue)
+    ab match {
+      case Left(err) => EvalData(Left(err), s)
+      case Right((Left(av), Left(bv))) => f(av, bv)
+      case _ => EvalData(Left(LogoEvaluationError(errMsg)), s)
+    }
+  }
+
+  def apply(expresion: Expresion, simbolos: SymbolTable): ValueResult = {
+    val epResult = expresion.ep.map(epEval(_, simbolos)).getOrElse(identity(_: ValueResult))
+    epResult(terminoEval(expresion.termino, simbolos))
+  }
+
+  private def nativaEval(nativa: NativaExpr, simbolos: SymbolTable): ValueResult = {
+    val exprValue: ValueResult = ExpresionEvaluator(nativa.parametro, simbolos)
+
+    exprValue match {
+      case Right(Left(num)) => nativa.funcion match {
+        case "random" => Right(Left(new Random(System.nanoTime()).nextInt(num)))
+      }
+      case Right(_) => Left(LogoEvaluationError("No se puede calcular una expresion nativa con una cadena como parametro"))
+      case _ => exprValue
+    }
+  }
+
+  private def factorEval(factor: Factor, simbolos: SymbolTable): ValueResult = factor match {
+    case Numero(n) => Right(Left(n))
+    case Variable(n) => simbolos.vars.get(n) match {
+      case Some(value) => Right(value)
+      case None => Left(LogoEvaluationError(s"la variable $n no está definida"))
+    }
+    case Cadena(c) => Right(Right(c))
+    case n: NativaExpr => nativaEval(n, simbolos)
+    case ParentesisExpr(expresion) => ExpresionEvaluator(expresion, simbolos)
+  }
+
+  private def tpEval(tp: Tp, simbolos: SymbolTable): ResultFunction = {
+    def computeLast(f: Factor, op: Operacion) = combineValues(op, _, factorEval(f, simbolos))
+    def compute(f: Factor, op: Operacion, s: Tp) = (combineValues(op, _, factorEval(f, simbolos))).andThen(tpEval(s, simbolos))
+    tp match {
+      case Por(f, None) => computeLast(f, Value.MULTIPLICACION)
+      case Por(f, Some(s)) => compute(f, Value.MULTIPLICACION, s)
+      case Dividido(f, None) => computeLast(f, Value.DIVISION)
+      case Dividido(f, Some(s)) => compute(f, Value.DIVISION, s)
+    }
+  }
+
+  private def terminoEval(termino: Termino, simbolos: SymbolTable): ValueResult = {
+    val tpResult = termino.tp.map(tpEval(_, simbolos)).getOrElse(identity(_: ValueResult))
+    tpResult(factorEval(termino.factor, simbolos))
+  }
+
+  private def epEval(ep: Ep, simbolos: SymbolTable): ResultFunction = {
+    def computeLast(t: Termino, op: Operacion) = combineValues(op, _, terminoEval(t, simbolos))
+    def compute(t: Termino, op: Operacion, s: Ep) = (combineValues(op, _, terminoEval(t, simbolos))).andThen(epEval(s, simbolos))
+    ep match {
+      case Mas(t, None) => computeLast(t, Value.SUMA)
+      case Mas(t, Some(s)) => compute(t, Value.SUMA, s)
+      case Menos(t, None) => computeLast(t, Value.RESTA)
+      case Menos(t, Some(s)) => compute(t, Value.RESTA, s)
+    }
+  }
+
   // TODO: Extraer esta prueba a un test unitario
   def main(args: Array[String]): Unit = {
     val result = ExpresionEvaluator(Expresion(Termino(Variable("hola"), None), Some(Mas(Termino(NativaExpr("random", Expresion(Termino(Numero(5),None),None)), None), None))), SymbolTable.empty.copy(vars = Map("hola" -> Left(4))))
@@ -21,58 +100,4 @@ object ExpresionEvaluator {
     }
   }
 
-  def apply(expresion: Expresion, simbolos: SymbolTable): ValueResult = {
-    val epResult = expresion.ep.map(epEval(_, simbolos)).getOrElse(identity(_: ValueResult))
-    epResult(terminoEval(expresion.termino, simbolos))
-  }
-
-  def nativaEval(nativa: NativaExpr, simbolos: SymbolTable): ValueResult = {
-    val exprValue: ValueResult = ExpresionEvaluator(nativa.parametro, simbolos)
-
-    exprValue match {
-      case Right(Left(num)) => nativa.funcion match {
-        case "random" => Right(Left(new Random(System.nanoTime()).nextInt(num)))
-      }
-      case Right(_) => Left(LogoEvaluationError("No se puede calcular una expresion nativa con una cadena como parametro"))
-      case _ => exprValue
-    }
-  }
-
-  def factorEval(factor: Factor, simbolos: SymbolTable): ValueResult = factor match {
-    case Numero(n) => Right(Left(n))
-    case Variable(n) => simbolos.vars.get(n) match {
-      case Some(value) => Right(value)
-      case None => Left(LogoEvaluationError(s"la variable $n no está definida"))
-    }
-    case Cadena(c) => Right(Right(c))
-    case n: NativaExpr => nativaEval(n, simbolos)
-    case ParentesisExpr(expresion) => ExpresionEvaluator(expresion, simbolos)
-  }
-
-  def tpEval(tp: Tp, simbolos: SymbolTable): ResultFunction = {
-    def computeLast(f: Factor, op: Operacion) = combineValues(op, _, factorEval(f, simbolos))
-    def compute(f: Factor, op: Operacion, s: Tp) = (combineValues(op, _, factorEval(f, simbolos))).andThen(tpEval(s, simbolos))
-    tp match {
-      case Por(f, None) => computeLast(f, Value.MULTIPLICACION)
-      case Por(f, Some(s)) => compute(f, Value.MULTIPLICACION, s)
-      case Dividido(f, None) => computeLast(f, Value.DIVISION)
-      case Dividido(f, Some(s)) => compute(f, Value.DIVISION, s)
-    }
-  }
-
-  def terminoEval(termino: Termino, simbolos: SymbolTable): ValueResult = {
-    val tpResult = termino.tp.map(tpEval(_, simbolos)).getOrElse(identity(_: ValueResult))
-    tpResult(factorEval(termino.factor, simbolos))
-  }
-
-  def epEval(ep: Ep, simbolos: SymbolTable): ResultFunction = {
-    def computeLast(t: Termino, op: Operacion) = combineValues(op, _, terminoEval(t, simbolos))
-    def compute(t: Termino, op: Operacion, s: Ep) = (combineValues(op, _, terminoEval(t, simbolos))).andThen(epEval(s, simbolos))
-    ep match {
-      case Mas(t, None) => computeLast(t, Value.SUMA)
-      case Mas(t, Some(s)) => compute(t, Value.SUMA, s)
-      case Menos(t, None) => computeLast(t, Value.RESTA)
-      case Menos(t, Some(s)) => compute(t, Value.RESTA, s)
-    }
-  }
 }
--- src/main/scala/parser/LogoCodeGenerator.scala
+++ src/main/scala/parser/LogoCodeGenerator.scala
@@ -1,16 +1,14 @@
 package parser
 
-import Monoid._
 import parser.EvalData.EvalResult
 import parser.LogoCompilationError.LogoEvaluationError
-import parser.Value.ValueResult
-
-import scala.util.Random
+import parser.Monoid._
 
 object LogoCodeGenerator {
 
-  import scala.util.{Right, Left}
   import SymbolTable._
+
+  import scala.util.{Left, Right}
   // registrar la posicion de la tortuga y el estado del pen
   // Asumo que no existen ámbitos. Todas las variables son globales y se pueden leer si están definidas previamente.
   // Una vez que se definen, permanecen definidas hasta el final de la ejecución
@@ -38,7 +36,72 @@ object LogoCodeGenerator {
     case p: LlamadaProcedimiento => ???
   }
 
-  def moverEval(mover: Mover, simbolos: SymbolTable): EvalData = ???
+  def moverEval(mover: Mover, simbolos: SymbolTable): EvalData = mover match {
+    case Forward(expr) => ExpresionEvaluator.evalNumber(expr, simbolos) { distancia: Int =>
+      val dx = distancia * Math.cos(simbolos.orientation.toRadians)
+      val dy = distancia * Math.sin(simbolos.orientation.toRadians)
+      val newX = (simbolos.position._1 + dx).toInt
+      val newY = (simbolos.position._2 + dy).toInt
+
+      val code = if(simbolos.isPenUp) s"moveTo($newX, $newY)" else s"lineTo($newX, $newY)"
+      EvalData(Right(code), simbolos.setPosition(newX, newY))
+    }
+    case Backward(expr) => ExpresionEvaluator.evalNumber(expr, simbolos) { distancia: Int =>
+      val dx = distancia * Math.cos(simbolos.orientation.toRadians)
+      val dy = distancia * Math.sin(simbolos.orientation.toRadians)
+      val newX = (simbolos.position._1 - dx).toInt
+      val newY = (simbolos.position._2 - dy).toInt
+
+      val code = if(simbolos.isPenUp) s"moveTo($newX, $newY)" else s"lineTo($newX, $newY)"
+      EvalData(Right(code), simbolos.setPosition(newX, newY))
+    }
+    case parser.Right(expr) => ExpresionEvaluator.evalNumber(expr, simbolos) { angulo: Int =>
+      val newSimbolos = simbolos.setOrientation(simbolos.orientation - angulo)
+      EvalData.withSymbols(newSimbolos)
+    }
+    case parser.Left(expr) => ExpresionEvaluator.evalNumber(expr, simbolos) { angulo: Int =>
+      val newSimbolos = simbolos.setOrientation(simbolos.orientation + angulo)
+      EvalData.withSymbols(newSimbolos)
+    }
+    case LeftArc(radio, angulo) => ExpresionEvaluator.eval2Numbers(radio, angulo, simbolos) { (r: Int, a: Int) =>
+      val anguloActual = simbolos.orientation
+      val anguloHaciaOrigen = (anguloActual + 90) % 360
+      val dx = (r * Math.cos(anguloHaciaOrigen.toRadians)).toInt
+      val dy = (r * Math.sin(anguloHaciaOrigen.toRadians)).toInt
+      val xArco = simbolos.position._1 + dx
+      val yArco = simbolos.position._2 + dy
+      val anguloInicial = (anguloHaciaOrigen + 180) % 360
+      val anguloFinal = (anguloInicial + a) % 360
+      val nuevaOrientacion = (anguloActual + a) % 360
+      val nuevoX = xArco + (r * Math.cos((anguloActual - 90 + a).toRadians)).toInt
+      val nuevoY = yArco + (r * Math.sin((anguloActual - 90 + a).toRadians)).toInt
+
+      val code = if(simbolos.isPenUp) s"moveTo($nuevoX, $nuevoY)" else s"arc($xArco, $yArco, $r, $anguloInicial, $anguloFinal)"
+      val newSymbols = simbolos.setPosition(nuevoX, nuevoY).setOrientation(nuevaOrientacion)
+      EvalData(scala.util.Right(code), newSymbols)
+    }
+    case RightArc(radio, angulo) => ExpresionEvaluator.eval2Numbers(radio, angulo, simbolos) { (r: Int, a: Int) =>
+      val anguloActual = simbolos.orientation
+      val anguloHaciaOrigen = (anguloActual - 90) % 360
+      val dx = (r * Math.cos(anguloHaciaOrigen.toRadians)).toInt
+      val dy = (r * Math.sin(anguloHaciaOrigen.toRadians)).toInt
+      val xArco = simbolos.position._1 + dx
+      val yArco = simbolos.position._2 + dy
+      val anguloInicial = (anguloHaciaOrigen + 180) % 360
+      val anguloFinal = (anguloInicial - a) % 360
+      val nuevaOrientacion = (anguloActual - a) % 360
+      val nuevoX = xArco + (r * Math.cos((anguloActual + 90 - a).toRadians)).toInt
+      val nuevoY = yArco + (r * Math.sin((anguloActual + 90 - a).toRadians)).toInt
+
+      val code = if(simbolos.isPenUp) s"moveTo($nuevoX, $nuevoY)" else s"arc($xArco, $yArco, $r, $anguloInicial, $anguloFinal)"
+      val newSymbols = simbolos.setPosition(nuevoX, nuevoY).setOrientation(nuevaOrientacion)
+      EvalData(scala.util.Right(code), newSymbols)
+    }
+    case Home => EvalData.withSymbols(simbolos.setPosition(0, 0))
+    case SetXY(x, y) => ExpresionEvaluator.eval2Numbers(x, y, simbolos, "No se pueden usar cadenas como posicion") {
+      (xv, yv) => EvalData.withSymbols(simbolos.setPosition(xv, yv))
+    }
+  }
 
   def comandoEval(comando: Comando, simbolos: SymbolTable): EvalData = comando match {
     case ClearScreen => EvalData(Right("clearScreen"), simbolos)
@@ -55,8 +118,11 @@ object LogoCodeGenerator {
 
   def buclesEval(bucles: Bucles, table: SymbolTable): EvalData = {
     def forEval(forLoop: For, simbolos: SymbolTable): EvalData = ???
+
     def repeatEval(repeat: Repeat, simbolos: SymbolTable): EvalData = ???
+
     def ifEval(ifClause: If, simbolos: SymbolTable): EvalData = ???
+
     ???
   }
 
